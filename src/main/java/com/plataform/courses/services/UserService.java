528533e74b26c93d91fd99eee57e66a69154fd1b
Vitor Daniel <vitordanieldeveloper@gmail.com>
2024-06-15 11:56:06 -0300

    Creating UserService

---
+++ src/main/java/com/plataform/courses/services/UserService.java
@@ -0,0 +1,48 @@
+package com.plataform.courses.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.plataform.courses.model.entity.User;
+import com.plataform.courses.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(()-> new RuntimeException(
+            "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try{
+            this.userRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
